ad843dbe3f91d930c77760d0cb9299eefca669b3
lauretteroy <laurieroy.dev@gmail.com>
2021-12-24 14:37:15 -0800

    Add article route

---
+++ spec/routing/articles_spec.rb
@@ -0,0 +1,9 @@
+
+RSpec.describe "/articles routes" do
+	it "routes to articles#index" do
+		aggregate_failures do
+			expect(get "/articles").to route_to("articles#index")
+			# expect(get "/articles?page[number]=3").to route_to("articles#index", page: { "number" => 3})
+		end
+	end
+end
